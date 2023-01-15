e6ef58688122025a27c5aeb8ef1e7add5dd1be8b
Trebolll <M-or-ev@yandex.ru>
2023-01-16 02:00:37 +0700

    add test files

--- src/main/java/service/comparator/interfaces/Comparator.java
+++ src/main/java/service/comparator/interfaces/Comparator.java
@@ -5,9 +5,7 @@ import io.reader.Reader;
 import java.util.Map;
 
 public interface Comparator {
-
     Map.Entry<Reader, ?> getAimEntry(Map<Reader, String> pool);
-
 
     String getValue(Reader flow);
 }
